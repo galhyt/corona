a8e55864305bc17915bbc067f31ae0d9cc996343
unknown <galhyt@gmail.com>
2020-03-31 19:27:23 +0300

    Updates

---
+++ precache-manifest.c6647c78f27577427441f2bdc5ba8c93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39630677116f259ee9b34cf4080173a0",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "153f5c0b1239072ccb3b",
+    "url": "/corona/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ab3ab3542e298121f748",
+    "url": "/corona/static/js/2.dca22fb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/corona/static/js/2.dca22fb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153f5c0b1239072ccb3b",
+    "url": "/corona/static/js/main.05eeed60.chunk.js"
+  },
+  {
+    "revision": "a3b787bfbf2c81f2e0d4",
+    "url": "/corona/static/js/runtime-main.c6168e55.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/corona/static/media/logo.5d5d9eef.svg"
+  }
+]);
